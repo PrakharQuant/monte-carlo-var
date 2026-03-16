58314f79059d58d5239bca86c76440ba1ecb5f7d
PrakharQuant <bestofprakhar@gmail.com>
2026-03-16 23:08:59 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,11 @@
+from .var_engine import (
+    download_data,
+    compute_returns,
+    run_monte_carlo,
+    calculate_var,
+    calculate_cvar,
+    calculate_historical_var,
+    stress_test,
+    build_var_table,
+)
 
